ba40ffc38e631a8ffb67eace77b7a06908fbd027
LBS IT <percylee@lbsgroup.com.hk>
2020-06-05 10:53:32 +0800

    Sync with AP production

---
+++ protected/messages/en/qc.php
@@ -0,0 +1,276 @@
+<?php
+return array(
+/*
+	'QC Record'=>'品检记录',
+	'QC List'=>'品检列表',
+	'QC Form'=>'品检表单',
+	'Quality Control'=>'品检',
+
+	'Date'=>'日期',
+	'Customer'=>'客户编号及名称',
+	'Staff-Job'=>'外勤',
+	'QC Date'=>'质检日期',
+	'Staff-QC'=>'质检部同事',
+    'QC Result'=>'总分',
+
+	'Entry Date'=>'输入日期',
+	'Team'=>'小组',
+	'Month'=>'月份',
+	'Resp. Staff'=>'外勤编号及名称',
+	'Input Date'=>'输入日期',
+	'Service Score'=>'服务评分',
+	'Customer Score'=>'客户评分',
+	'Customer Comment'=>'客户意见',
+	'QC Comment'=>'质检员意见',
+	'QC Result'=>'质检成绩',
+	'Env. Grade'=>'现场质检环境',
+	'Signatures'=>'签名',
+	'Signature'=>'客户签名',
+	'QC Staff'=>'质检部同事编号及名称',
+	'Remarks'=>'备注',
+	'Service Type'=>'服务类别',
+	'Machine Score'=>'机器评分',
+	'Toilet Score'=>'洁具评分',
+	'Sink Score'=>'洗手盆评分',
+	'Toilet Sticker'=>'洁具贴',
+	'Machine Sticker'=>'机器贴',
+	'Large Sticker'=>'大标贴',
+	'According to customer request on hand-washing/flushing sticker'=>'洗手贴/冲厕贴，根据客户要求',
+	'Sticker Request No.'=>'场内应贴有',
+	'Sticker Actual No.'=>'场内只贴有',
+	'Sticker Score'=>'标贴评分',
+	'Enzyme Score'=>'除菌液、酵素评分',
+	'Blue Card Score'=>'常用瓶、蓝卡评分',
+	'Need to Improve'=>'需改善地方',
+	'Praise'=>'有赞扬地方',
+	'Customer Signature'=>'客户签名',
+	'Technician Signature'=>'技术员签名',
+	'QC Signature'=>'质检员签名',
+	'Missing'=>'欠',
+	'Broken'=>'残',
+	'Service Date'=>'服务日期',
+	'Normal'=>'正常场所',
+	'Poor'=>'较差场所',
+	'Worst'=>'恶劣场所',
+	'Invalid Value'=>'数值不正确',
+	'is not between 0 and '=>'不能小于0或大于',
+
+	'Personal Image'=>'个人形象',
+	'Uniform'=>'着装规范',
+	'Equipment'=>'装备齐全',
+	'Communication Skill'=>'沟通技巧',
+	'Greeting'=>'进门打招呼',
+	'On-site Communication'=>'现场沟通',
+	'Rat'=>'鼠防治',
+	'Rat Freq.'=>'鼠密度',
+	'Rat Checking'=>'现场检查',
+	'Rat Disposal'=>'死鼠清理',
+	'Sticky Board'=>'粘板布放',
+	'Hole Closure'=>'鼠洞封堵',
+	'Warning Label'=>'警示标签',
+	'Drug (Rat)'=>'鼠药投放',
+	'Cockroach'=>'蟑螂防治',
+	'Cockroach Freq.'=>'蟑螂密度',
+	'Cockroach Checking'=>'现场检查',
+	'Drug (Cockroach)'=>'胶饵点施',
+	'Expired Drug'=>'失效胶饵的处理',
+	'Toxin'=>'撒施毒饵',
+	'Fly'=>'飞虫防治',
+	'Fly Freq.'=>'飞虫密度',
+	'Fly Cup'=>'蚊滋杯',
+	'Fly Lamp'=>'灭蚊蝇灯',
+	'Fly Control'=>'滋生地处理',
+	'Spray'=>'化学剂喷洒',
+	'Safety'=>'安全',
+	'Safety Score'=>'安全评分',
+	'High'=>'高',
+	'Medium'=>'中',
+	'Low'=>'低',
+	'Photo Attachment'=>'照片附件',
+	'Photo with Cust.'=>'客户合照',
+	'QC Score'=>'质检评分',
+	'(60% of Total Score)'=>'(质检评分占总分60%)',
+	'(40% of Total Score)'=>'(服务评分占总分40%)',
+	'Total Score'=>'总分',
+	'Effect Score'=>'工作后效果',
+*/	
+	
+	'desc101'=>'Grade A：Good Venue (Water Flow+No Blockage)<br> 
+Grade B：Avg. Venue (Grade B：Avg. Venue)<br>
+Grade C：Poor Venue (No Water Flow+Blockage)
+',
+	'desc108'=>'Fan/Spray/A100	(Full Scores:9pt)<br>
+- Thin layer of Dust (Score 8pt)<br>
+- Thick layer of Dust，Stains，Fagrance below 1/2，No paper Core (Score 7pt)<br>
+- No maintance，Fagrance ，Paper core，Spray Can (Score 0pt)<br>
+<br>
+Paper Dispenser  (Full Scores:2pt)<br>
+M Fold/CPT/JRT Dispensers<br>
+- Thin layer of Dust (Score 1pt)<br>
+- Thick layer of Dust，Stains (Score 0pt)<br>
+<br>
+All kinds of Soap Dispensers (Full Scores:3pt)<br>
+- Thin layer of Dust (Score 2pt)<br>
+- Thick layer of Dust，Stains (Score 1pt)<br>
+- No Soap，Press button or injector disabled  (Score 0pt)
+',
+	'desc105'=>'Wash Basin-Stains（6）<br>
+(Surface/Inner Corner Edge/Drainage Ring/Drainage Hole/Inner Wall/Faucet/
+Periphery/Under Basin/Water Barrier)<br>
+- 1 Item 4pts<br>
+- 2-3 Items 3pts<br>
+- 4-6 Items 2pts<br>
+- 7-9 Items 0pts<br>
+*Serious stains will score 0pts
+',
+	'desc106'=>'Fixtures<br>
+1. Toilet Bowl（25）<br>
+(Water Tank/Seat&Lid/Hinge/Periphery/Inlier/Drainage Holes/Inlier Pit Side/
+Main Drainage/Back Column)<br>
+- Stains Grade:N/A	(Score 25pt)<br>
+- Stones Level Grade:Low (Score 15pt)<br>
+- Stones Level Grade:Medium (Score 10pt)<br>
+- Stones Level Grade:High (Score 5pt)<br>
+- Stones Level Grade:Extreme (Score 0pt)<br>
+<br>
+2. Urinal Bow（25）<br>
+(Periphery/Inlier Surface/Drainage Holes/Perforated plate/
+Down Drainage/Down Pipe/Urinal Screen/Bottom Parts)<br>
+- Stains Grade:N/A	(Score 25pt)<br>
+- Stones Level Grade:Low (Score 20pt)<br>
+- Stones Level Grade:Medium (Score 15pt)<br>
+- Stones Level Grade:High (Score 10pt)<br>
+- Stones Level Grade:Extreme (Score 5pt)
+',
+	'desc201'=>'
+Clothing Standard（Well presented，Proper dressing for work）Add points (10)<br>
+（1）Clothing、Work Badge、shoes (5pt)<br>  
+（2）Standard dressing? (5pt)<br>
+
+	',
+	'desc202'=>'
+Appropriate Tools（Check the completeness of Tools and chemicals）Add points (10)<br>
+（1）Complete Tool kit set (10pt)<br>
+	',
+	'desc203'=>'
+Greet when Entering（Technicians are required to report duty to the person in charge of the shop before servicing）Points Deduction (20)<br>
+（1）Without greeting  (10pt)<br>  
+（2）Greeting with wrong procedures (10pt)<br>
+	',
+	'desc204'=>'
+On site Communication（Technicians’ communication and inquire with on-site workers）Add points (20)<br>
+（1）Updating for current situations (10pt)<br>  
+（2）Replying complaints with proper explanation (5pt)<br>
+（3）Showing the result and application to the customer after service (5pt)<br>
+	',
+	'desc205'=>'
+Inspection（For appropriate working procedures）Add Points (25)<br>
+（1）Trap placing in correct angle (8pt)<br>  
+（2）Rooftop (7pt)<br>
+（3）Storage (5pt)<br>
+（4）Fume Hood (5pt)
+	',
+	'desc206'=>'
+Rodent Trap Set Up（Placing in a proper position，replacing invalid traps on time Points Deduction (15)<br>
+（1）Not inspected (5pt)<br>  
+（2）Invalid and not replaced (5pt)<br>
+（3）Placed in a non-proper position (5pt)
+	',
+	'desc207'=>'
+Handle of Dead Rodent（Inspect and clean up dead rodents on time）Points Deduction (15)<br>
+（1）Not recognized dead rodent on the trap (8pt)<br>  
+（2）Not recognize dead dried rodent bodies on the rooftop (=7pt)<br>
+	',
+	'desc208'=>'
+Rodent holes blockage（Blocking for all possible entry points and without miss inspection ）Points Deduction (15)<br>
+（1）Possible entry point but without blocking (5pt)<br>  
+（2）Miss inspected blockag (5pt)<br>
+（3）Disabled blockage without maintenance (5pt)
+	',
+	'desc209'=>'
+Warning labels（Inspections of proper warning labeling）Points Deduction (15)<br>
+（1）Without warning labeling (10pt)<br>  
+（2）Disabled labeling、Invalid and not replaced  (5pt)<br>
+	',
+	'desc210'=>'
+Baits Placement（Fully covered of baits placing，and refill on time）Points Deduction (15)<br>
+（1）Placing spots without inspections (5pt)<br>  
+（2）Invalid baits without replacing (5pt)<br>
+（3）Improper baits placing spot (5pt)
+	',
+	'desc211'=>'
+Inspection（For appropriate working procedures）Add Points (25)<br>
+（1）Clear direction for baiting (5pt)<br>  
+（2）Fridges and electronics equipment (5pt)<br>
+（3）Fire box/Hose station (5pt)<br>
+（4）Dinning Areas (5pt)<br>
+（5）Doors & Windows gap (5pt)<br>
+	',
+	'desc212'=>'
+Baits Placement（Fully cover of baits and proper placing）Add Points (25)<br>
+（1）Proper size of the baits (Green beans alike) (5pt)<br>  
+（2）Fully coverage of baiting (5pt)<br>
+（3）Placing in Hidden areas (5pt)<br>
+（4）Re-baits on time (5pt)<br>
+（5）Usage of torch (5pt)<br>
+	',
+	'desc213'=>'
+Deal with invalid baits（Replacing of outdated or invalid baits）Add Points (25)<br>
+（1）Replacing of outdated or invalid baits (13pt)<br>  
+（2）Proper of Replacing baits to prevent resistance (12pt)<br>
+	',
+	'desc214'=>'
+Powder Baits Placement（Fully cover of baits and proper placing）Add Points (25)<br>
+（1）Placing on dry areas (5pt)<br>  
+（2）Over placing of baits (5pt)<br>
+（3）Placing in Hidden areas (5pt)<br>
+（4）Re-baits on time (5pt)<br>
+（5）Using an 8oz bottle (5pt)<br>
+	',
+	'desc215'=>'
+Flies Baits Catcher（Proper set up of the catcher，Displaying of warning label，Replace in time）Add Points (25)<br>
+（1）Proper set up of the catcher (5pt)<br>  
+（2）Displaying of warning label (5pt)<br>
+（3）Set up with company authorized pape (5pt)<br>
+（4）Placing in a proper position (5pt)<br>
+（5）Replacing invalid labels (5pt)<br>
+	',
+	'desc216'=>'
+Light Traps（Replacing stick pad in time，proper placing of the stick pad）Add Points (25)<br>
+（1）Replacing the stick pad (10pt)<br>  
+（2）Proper placing of the stick pad (5pt)<br>
+（3）Proper inspection (10pt)<br>
+	',
+	'desc217'=>'
+Breeding Spot（Proper application to the bleeding spot）Add Points (25)<br>
+（1）Identify the correct bleeding spot (8pt)<br>  
+（2）Suggestion to customers form clean up (8pt)<br>
+（3）Apply proper treatment (9pt)<br>
+	',
+	'desc218'=>'
+Chemical usage（Apply proper procedures of spraying）Add Points (25)<br>
+（1）Correct mixing ratio (Chemical/Water) (5pt)<br>  
+（2）Proper standard for spray (5pt)<br>
+（3）Wastage of Chemicals? (5pt)<br>
+（4）Spray on the correct spot (5pt)<br>
+（5）Proper handle on the left over chemicals (5pt)<br>
+	',
+	'desc219'=>'
+Safety Rating（Proper placing of rodent bait、Avoid baits contact with food）Add points (10）<br>
+（1）Rodent baits placing on bait box 、Avoid baits contact with food (10pt)					
+',
+	'desc220'=>'Very Satisfactory （10 ）<br>
+Satisfactory （ 8 ）<br>
+Good     （ 5 ）<br>
+Unsatisfactory （ 3 ）<br>
+Poor （ 0 ）	
+',
+    'desc221'=>'
+Service Effectiveness (30)<br>
+（1）Rodent droppings and markings  N/A 10pt  2spot 2pt  4spot 4pt  6spot 8pt <br>  
+（2）Cockroach N/A 10pt 2pcs 2pt 4pcs 4pt 8pcs 8pt 10pcs or above 10pt （Half Marks for American Cockroach）<br>
+（3）Files、Fruit Files N/A 10pt 4pcs 2pt 8pcs 4pt 10pcs 6pt 12pcs 8pt  14pcs or above 10pt<br>
+
+	',
+);
+?>
