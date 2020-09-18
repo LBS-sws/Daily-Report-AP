0f435273b2ed0e3af0e94c42ac68a6e491a5a56b
anxiaxia <1286571499@qq.com>
2020-09-18 09:59:04 +0800

    客户资料表单新增发票相关栏位

--- protected/models/CustomerForm.php
+++ protected/models/CustomerForm.php
@@ -13,9 +13,12 @@ class CustomerForm extends CFormModel
 	public $nature;
 	public $address;
 	public $tax_reg_no;
+    public $delivery_company;
+    public $delivery_tel;
+    public $delivery_address;
 
 	public $service = array();
-	
+
 	/**
 	 * Declares customized attribute labels.
 	 * If not declared here, an attribute would have a label that is
@@ -32,16 +35,19 @@ class CustomerForm extends CFormModel
 			'cont_phone'=>Yii::t('customer','Contact Phone'),
 			'address'=>Yii::t('customer','Address'),
             'tax_reg_no'=>Yii::t('code','SSM No.'),
+            'delivery_company'=>Yii::t('customer','Delivery Company'),
+            'delivery_tel'=>Yii::t('customer','Delivery Tel'),
+            'delivery_address'=>Yii::t('customer','Delivery Address'),
 		);
 	}
-	
+
 	/**
 	 * Declares the validation rules.
 	 */
 	public function rules()
 	{
 		return array(
-			array('id, full_name, cont_name, cont_phone, address, tax_reg_no','safe'),
+			array('id, full_name, cont_name, cont_phone, address, tax_reg_no,delivery_company,delivery_tel,delivery_address','safe'),
 			array('name, code','required'),
 /*
 			array('code','unique','allowEmpty'=>true,
@@ -90,13 +96,16 @@ class CustomerForm extends CFormModel
 				$this->cont_phone = $row['cont_phone'];
 				$this->address = $row['address'];
 				$this->tax_reg_no = $row['tax_reg_no'];
+                $this->delivery_company = $row['delivery_company'];
+                $this->delivery_tel = $row['delivery_tel'];
+                $this->delivery_address = $row['delivery_address'];
 				break;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public function saveData()
 	{
 		$connection = Yii::app()->db;
@@ -120,10 +129,10 @@ class CustomerForm extends CFormModel
 				break;
 			case 'new':
 				$sql = "insert into swo_company(
-							code, name, full_name, tax_reg_no, cont_name, cont_phone, address,
+							code, name, full_name, tax_reg_no, cont_name, cont_phone, address,delivery_company,delivery_tel,delivery_address,
 							city, luu, lcu
 						) values (
-							:code, :name, :full_name, :tax_reg_no, :cont_name, :cont_phone, :address,
+							:code, :name, :full_name, :tax_reg_no, :cont_name, :cont_phone, :address,:delivery_company,:delivery_tel,:delivery_address,
 							:city, :luu, :lcu
 						)";
 				break;
@@ -136,6 +145,9 @@ class CustomerForm extends CFormModel
 							cont_name = :cont_name, 
 							cont_phone = :cont_phone, 
 							address = :address, 
+							delivery_company = :delivery_company,
+							delivery_tel = :delivery_tel,
+							delivery_address = :delivery_address,
 							luu = :luu 
 						where id = :id and city = :city
 						";
@@ -144,7 +156,7 @@ class CustomerForm extends CFormModel
 
 		$city = Yii::app()->user->city();
 		$uid = Yii::app()->user->id;
-		
+
 		$command=$connection->createCommand($sql);
 		if (strpos($sql,':id')!==false)
 			$command->bindParam(':id',$this->id,PDO::PARAM_INT);
@@ -162,6 +174,12 @@ class CustomerForm extends CFormModel
 			$command->bindParam(':cont_phone',$this->cont_phone,PDO::PARAM_STR);
 		if (strpos($sql,':address')!==false)
 			$command->bindParam(':address',$this->address,PDO::PARAM_STR);
+        if (strpos($sql,':delivery_company')!==false)
+            $command->bindParam(':delivery_company',$this->delivery_company,PDO::PARAM_STR);
+        if (strpos($sql,':delivery_tel')!==false)
+            $command->bindParam(':delivery_tel',$this->delivery_tel,PDO::PARAM_STR);
+        if (strpos($sql,':delivery_address')!==false)
+            $command->bindParam(':delivery_address',$this->delivery_address,PDO::PARAM_STR);
 		if (strpos($sql,':city')!==false)
 			$command->bindParam(':city',$city,PDO::PARAM_STR);
 		if (strpos($sql,':lcu')!==false)
